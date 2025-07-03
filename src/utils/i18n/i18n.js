9cfa3c91d1c17a984d3968bda02c967e8e97b94b
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-03 16:37:16 +0300

    read transation files from locales

--- src/utils/i18n/i18n.js
+++ src/utils/i18n/i18n.js
@@ -1,41 +1,45 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  en: {
-    translation: {
-      welcomeToReact: "Welcome to React and react-i18next",
-    },
-  },
-  ar: {
-    translation: {
-      welcomeToReact: "Bienvenue à React et react-i18next",
-    },
-  },
-};
+
+
+// const resources = {
+//   en: {
+//     translation: ,
+//   },
+//   ar: {
+//     translation: ,
+//   },
+// };
 
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  // passes i18n down to react-i18next
   .init({
-    resources,
+    // resources,
     // lng: "en",
     fallbackLng: "en",
     detection: {
       order: [
+        "htmlTag",
         "cookie",
         "localStorage",
         "sessionStorage",
         "navigator",
-        "htmlTag",
         "path",
         "subdomain",
       ],
       caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
     // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
