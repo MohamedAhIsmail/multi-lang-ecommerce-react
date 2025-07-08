27a1becb86f9e58c5382a0b4cbe969108f6d78a3
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-08 23:47:37 +0300

    add: new font, silder pagination

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/bundle";
 import IphoneSlide from "./IphoneSlide";
 import HeadphoneSlide from "./HeadphoneSlide";
 import IpadSlide from "./IpadSlide";
@@ -7,10 +9,14 @@ import IpadSlide from "./IpadSlide";
 function Slider() {
   return (
     <Swiper
+      modules={[Navigation, Pagination]}
       spaceBetween={50}
       slidesPerView={1}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
+      autoplay={true}
+      navigation={true}
+      pagination={{ clickable: true }}
     >
       <SwiperSlide>
         <IphoneSlide />
