5c7e5e42773aaa8af0bb2b4d1a363bd2fb6d3c43
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-09 21:46:27 +0300

    Revert "update: main header padding"
    This reverts commit a515b13addf33ef34f31e4af08238ce88db86384.

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -11,7 +11,7 @@ function MainHeader() {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <div className="bg-background-second pb-8">
+    <div className="bg-background-second">
       <div className="container relative bg-background-main px-5 py-4 rounded-full shadow-md flex justify-between items-center">
         <NavMenu setOpenMenu={setOpenMenu} />
         <Logo />
