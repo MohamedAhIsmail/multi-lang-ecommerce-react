ce13620386fdf3a28d88e8a9d70afdb59bd203c6
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-11 00:27:24 +0300

    add: Shadcn ui with tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./utils/i18n/i18n.js";
-import App from "./App.jsx";
-import "./styles/index.css";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
+import App from "./App.jsx";
+import "./utils/i18n/i18n.js";
+import "./styles/index.css";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
