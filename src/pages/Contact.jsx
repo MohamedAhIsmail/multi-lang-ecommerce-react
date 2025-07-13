a1a76b8af507e7c44e01336faebeff588464da5f
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-13 16:28:14 +0300

    fix: language detection order to read languge  from html tag instead of path and navigator

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useLanguage } from "@/hooks/useLanguage";
+import { useEffect, useState } from "react";
 
 function Contact() {
+  const [categories, setCategories] = useState([]);
+  const { language } = useLanguage() || "en";
+  console.log("lang is:",language);
+
+  // getCategories();
+
+  useEffect(() => {
+    async function getCategories() {
+      const res = await fetch(
+        `http://localhost:1337/api/categories?locale=${language}`
+      );
+      const { data } = await res.json();
+      setCategories(data);
+      console.log(data);
+    }
+    getCategories();
+  }, [language]);
+
   return (
-    <div>Contact</div>
-  )
+    <div>
+      {categories.map((cat) => (
+        <p>{cat.name}</p>
+      ))}
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
