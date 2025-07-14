f704c00504c02548f6ea29f6e9e6e12bb4b5b63b
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-14 18:38:07 +0300

    add: QueryClientProvider and React dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import "./utils/i18n/i18n.js";
 import "./styles/index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DarkModeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
     </DarkModeProvider>
   </StrictMode>
 );
